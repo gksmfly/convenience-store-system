3ff907c2be20323782894042658ae4ae84854140
gksmfly <skyhaneul0738@naver.com>
2025-10-16 16:41:20 +0900

    작업 내용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
-group = "com.bible"
-version = "1.0.0"
-
 plugins {
     kotlin("jvm") version "2.2.0"
     application
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
+
+group = "com.bible"
+version = "1.0.0"
 
 repositories { mavenCentral() }
 
@@ -14,24 +14,24 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-kotlin {
-    // 과제 명시: JDK 24
-    jvmToolchain(24)
-}
-
-tasks.test { useJUnitPlatform() }
-
-tasks.withType<JavaExec> {
-    standardInput = System.`in`
-}
+kotlin { jvmToolchain(24) }
 
 application {
+    // App.kt에 fun main() 이고 package store 라면 이게 맞음
     mainClass.set("store.AppKt")
     applicationDefaultJvmArgs = listOf("-Dfile.encoding=UTF-8")
+}
+
+tasks.jar {
+    enabled = false
 }
 
 tasks.shadowJar {
     archiveBaseName.set("convenience-store-system")
     archiveVersion.set("1.0.0")
-    archiveClassifier.set("")
+    archiveClassifier.set("")                         // ← -all 제거
+    manifest { attributes["Main-Class"] = application.mainClass.get() }
+    mergeServiceFiles()
 }
+
+tasks.build { dependsOn(tasks.shadowJar) }
